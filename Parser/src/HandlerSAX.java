29e9d8837a5dd2a17bc8fcd5d52f48ee169d4ea9
Lionel-Driessens <78431720+Lionel-Driessens@users.noreply.github.com>
2021-11-13 13:07:13 +0100

    Add files via upload

--- Parser/src/HandlerSAX.java
+++ Parser/src/HandlerSAX.java
@@ -1,75 +1,90 @@
 import org.xml.sax.SAXException;
+
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.util.Vector;
 
-
-
-public class HandlerSAX extends DefaultHandler {
-    int cptTag = 0, cptCertif = 0, j = -1;
+public class HandlerSAX extends DefaultHandler{
+    int cptTag = 0, cptCertif=0, j = -1;
     Double note1, note2;
     boolean title = false, vote = false;
 
-    Vector<Vector<String>> classement = new Vector<Vector<String>>();
-    Vector<String> film = new Vector<String>();
-    Vector<String> temp, temp1, temp2;
+    Vector classement = new Vector();
+    Vector film = new Vector();
+    Vector temp, temp1, temp2;
 
-    public void characters(char[] ch, int start, int length) throws SAXException {
+    public void characters(char[] ch, int start, int length) throws SAXException{
         String chaine = new String(ch,start,length).trim();
-        if(chaine.length() > 0) {
+        if(chaine.length()>0)
+        {
             System.out.println("Caratères: " + chaine);
 
             if(chaine.equals("PG-13"))
+            {
                 cptCertif++;
-
-            if(title == true) {
-                film = new Vector<String>();
-                film.add(chaine);
             }
 
-            if(vote == true) {
+            if(title == true)
+            {
+                film = new Vector();
                 film.add(chaine);
-                j =- 1;
-                for(int i = 0; i < 10; i++) {
-                    temp = (Vector<String>)classement.get(i);
-                    note1 = Double.parseDouble((String)temp.get(1));
-                    note2 = Double.parseDouble((String)film.get(1));
+            }
+            if(vote == true)
+            {
+                film.add(chaine);
+                j=-1;
+                for(int i=0 ; i<10 ; i++)
+                {
+                    temp = (Vector) classement.get(i);
+                    note1 = Double.parseDouble((String) temp.get(1));
+                    note2 = Double.parseDouble((String) film.get(1));
                     if (note1 < note2)
-                        j = i;
+                    {
+                        j=i;
+                    }
                     else
+                    {
                         break;
+                    }
                 }
-
-                if(j != -1) {
-                    int k = j;
-                    temp1 = (Vector<String>)classement.get(k);
-                    for(; k >= 0; k--) {
-                        if(k-1 != -1) {
-                            temp2 = (Vector<String>)classement.get(k-1);
-                            classement.set(k-1, temp1);
+                if(j!=-1)
+                {
+                    int k=j;
+                    temp1 = (Vector)  classement.get(k);
+                    for( ; k>=0 ; k--)
+                    {
+                        if(k-1!=-1)
+                        {
+                            temp2 = (Vector)  classement.get(k-1);
+                            classement.set(k-1,temp1);
                         }
-                        temp1 = temp2;
+                        temp1=temp2;
                     }
 
-                    classement.set(j, film);
+                    classement.set(j,film);
                 }
+
+
             }
         }
     }
 
-    public void startDocument() throws SAXException {
+    public void startDocument() throws SAXException{
+
         film.add("Name");
         film.add("0.0");
 
-        for(int i = 0; i < 10; i++)
+        for(int i=0 ; i<10 ; i++)
+        {
             classement.add(film);
+        }
 
         System.out.println("-- Début du document --");
     }
-
-    public void endDocument() throws SAXException {
+    public void endDocument() throws SAXException{
         System.out.println("-- Fin du document --");
 
         System.out.println("Nbr PG-13: " + cptCertif);
@@ -77,19 +92,24 @@ public class HandlerSAX extends DefaultHandler {
         System.out.println("classement: " + classement);
     }
 
-    public void startElement(java.lang.String uri, java.lang.String localName, java.lang.String qName, Attributes attr) throws SAXException {
+    public void startElement(java.lang.String uri, java.lang.String localName, java.lang.String qName, Attributes attr) throws SAXException{
         System.out.println("- Début d'une balise");
 
         cptTag++;
 
         if(uri != null && uri.length() > 0)
+        {
             System.out.println("uri: " + uri);
+        }
 
-        if(qName != null && qName.length() > 0) {
+        if(qName != null && qName.length() > 0)
+        {
             System.out.println("nom complet: " + qName);
 
             if(qName.equals("title"))
+            {
                 title = true;
+            }
 
             if(qName.equals("voteAverage"))
                 vote = true;
@@ -97,30 +117,35 @@ public class HandlerSAX extends DefaultHandler {
 
         int nAttr = attr.getLength();
         System.out.println("nombre d'attributs: " + nAttr);
-        if(nAttr == 0)
+        if(nAttr==0)
+        {
             return;
+        }
 
-        for(int i = 0; i < nAttr; i++)
+        for(int i=0 ; i<nAttr ; i++)
+        {
             System.out.println("attribut n°" + i + "->" + attr.getLocalName(i) + "avec valeur: " + attr.getValue(i));
+        }
     }
 
-    public void endElement(java.lang.String uri, java.lang.String localName, java.lang.String qName) throws SAXException {
+    public void endElement(java.lang.String uri, java.lang.String localName, java.lang.String qName) throws SAXException{
         System.out.println("- Fin de la balise");
 
         cptTag++;
 
         title = false;
         vote = false;
+
     }
 
-
-    public void warning( SAXParseException e) throws SAXException {
+    public void warning( SAXParseException e) throws SAXException{
         System.out.println(("WARNING : "+ e.getMessage ()));
     }
-    public void error( SAXParseException e) throws SAXException {
+
+    public void error( SAXParseException e) throws SAXException{
         System.out.println(("ERROR : "+ e.getMessage ()));
     }
-    public void fatalError( SAXParseException e) throws SAXException {
+    public void fatalError( SAXParseException e) throws SAXException{
         System.out.println(("FATAL : "+ e.getMessage ()));
     }
 }
--- Parser/src/SimplestSaxParser.java
+++ Parser/src/SimplestSaxParser.java
@@ -4,28 +4,29 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.SAXException;
 
-
 public class SimplestSaxParser {
+
     public static void main(String[] args) {
         try {
+
             SAXParserFactory factory = SAXParserFactory.newInstance();
 
             factory.setValidating(true);
             factory.setNamespaceAware(true);
 
             SAXParser sp = factory.newSAXParser();
-            sp.parse(GetNomFichier("films.xml"), new HandlerSAX());
+            sp.parse("C:\\Users\\dries\\IdeaProjects\\XML\\Fichiers\\films.xml", new HandlerSAX());
+
         }
         catch (ParserConfigurationException pce) {
             System.out.println("ParserConfigurationException : " + pce.getMessage());
-        } catch (IOException io) {
+        }
+        catch (IOException io) {
             System.out.println("IOException : " + io.getMessage());
-        } catch (SAXException se){
+        }
+        catch (SAXException se){
             System.out.println("SaxEXception : " + se.getMessage());
         }
     }
 
-    public static String GetNomFichier(String nomf) {
-        return System.getProperty("user.dir") + System.getProperty("file.separator") + "XML" + System.getProperty("file.separator") + "Fichiers" + System.getProperty("file.separator") + nomf;
-    }
 }
