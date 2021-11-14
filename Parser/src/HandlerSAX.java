481bc48b79605ecf32db052a6359c6979710f26f
Ryuggo <hugofrancois12@gmail.com>
2021-11-14 12:51:59 +0100

    SAX & DOM verifier done

--- Parser/src/HandlerSAX.java
+++ Parser/src/HandlerSAX.java
@@ -11,12 +11,10 @@ public class HandlerSAX extends DefaultHandler {
     boolean show = false;
 
     int cptTag = 0, cptCertif = 0, j = -1;
-    Double note1, note2;
     boolean title = false, vote = false;
 
     Vector<Vector<String>> classement = new Vector<Vector<String>>();
     Vector<String> film = new Vector<String>();
-    Vector<String> temp, temp1, temp2;
 
     public void characters(char[] ch, int start, int length) throws SAXException {
         String chaine = new String(ch, start, length).trim();
@@ -32,13 +30,32 @@ public class HandlerSAX extends DefaultHandler {
                 film.add(chaine);
             }
 
+            
+            /*
+            for(int i = 0, j; i < movies.getLength(); i++) {
+                NodeList tmp = movies.item(i).getChildNodes();
+                for(j = 0; j < 10; j++)
+                    if(Double.parseDouble(tmp.item(5).getTextContent()) <= Double.parseDouble(classement[j][1]))
+                        break;
+
+                if(0 < j) {
+                    for(int k = 0; k < j-1; k++) {
+                        classement[k][0] = classement[k+1][0];
+                        classement[k][1] = classement[k+1][1];
+                    }
+                    classement[j-1][0] = tmp.item(2).getTextContent();
+                    classement[j-1][1] = tmp.item(5).getTextContent();
+                }
+            }
+            */
+
             if(vote == true) {
                 film.add(chaine);
-                j =- 1;
+                j = -1;
                 for(int i = 0; i < 10; i++) {
-                    temp = (Vector<String>)classement.get(i);
-                    note1 = Double.parseDouble((String)temp.get(1));
-                    note2 = Double.parseDouble((String)film.get(1));
+                    Vector<String> tmp = classement.get(i);
+                    Double note1 = Double.parseDouble((String)tmp.get(1));
+                    Double note2 = Double.parseDouble((String)film.get(1));
                     if (note1 < note2)
                         j = i;
                     else
@@ -47,13 +64,13 @@ public class HandlerSAX extends DefaultHandler {
 
                 if(j != -1) {
                     int k = j;
-                    temp1 = (Vector<String>)classement.get(k);
-                    for(; k >= 0; k--) {
+                    Vector<String> tmp1 = classement.get(k);
+                    for(Vector<String> tmp2 = null; k >= 0; k--) {
                         if(k-1 != -1) {
-                            temp2 = (Vector<String>)classement.get(k-1);
-                            classement.set(k-1, temp1);
+                            tmp2 = classement.get(k-1);
+                            classement.set(k-1, tmp1);
                         }
-                        temp1 = temp2;
+                        tmp1 = tmp2;
                     }
 
                     classement.set(j, film);
@@ -77,7 +94,9 @@ public class HandlerSAX extends DefaultHandler {
 
         System.out.println("Nbr PG-13: " + cptCertif);
         System.out.println("compteur de tag: " + cptTag);
-        System.out.println("classement: " + classement);
+        System.out.println("classement: ");
+        for(int i = 0; i < 10; i++)
+            System.out.println(classement.get(i));
     }
 
     public void startElement(java.lang.String uri, java.lang.String localName, java.lang.String qName, Attributes attr) throws SAXException {
@@ -89,8 +108,9 @@ public class HandlerSAX extends DefaultHandler {
         if(uri != null && uri.length() > 0 && show)
             System.out.println("uri: " + uri);
 
-        if(qName != null && qName.length() > 0 && show) {
-            System.out.println("nom complet: " + qName);
+        if(qName != null && qName.length() > 0) {
+            if(show)
+                System.out.println("nom complet: " + qName);
 
             if(qName.equals("title"))
                 title = true;
--- Parser/src/SimplestDomParser.java
+++ Parser/src/SimplestDomParser.java
@@ -42,17 +42,16 @@ public class SimplestDomParser {
             for(int i = 0, j; i < movies.getLength(); i++) {
                 NodeList tmp = movies.item(i).getChildNodes();
                 for(j = 0; j < 10; j++)
-                    if(Double.parseDouble(tmp.item(5).getTextContent()) < Double.parseDouble(classement[j][1]))
+                    if(Double.parseDouble(tmp.item(5).getTextContent()) <= Double.parseDouble(classement[j][1]))
                         break;
 
-                j--;
                 if(0 < j) {
                     for(int k = 0; k < j-1; k++) {
                         classement[k][0] = classement[k+1][0];
                         classement[k][1] = classement[k+1][1];
                     }
-                    classement[j][0] = tmp.item(2).getTextContent();
-                    classement[j][1] = tmp.item(5).getTextContent();
+                    classement[j-1][0] = tmp.item(2).getTextContent();
+                    classement[j-1][1] = tmp.item(5).getTextContent();
                 }
             }
 
@@ -60,36 +59,6 @@ public class SimplestDomParser {
             System.out.println("Classement : ");
             for(int i = 0; i < 10; i++)
                 System.out.println(classement[i][0] + " - " + classement[i][1]);
-
-            /*
-            if(vote == true) {
-                film.add(chaine);
-                j =- 1;
-                for(int i = 0; i < 10; i++) {
-                    temp = (Vector<String>)classement.get(i);
-                    note1 = Double.parseDouble((String)temp.get(1));
-                    note2 = Double.parseDouble((String)film.get(1));
-                    if (note1 < note2)
-                        j = i;
-                    else
-                        break;
-                }
-
-                if(j != -1) {
-                    int k = j;
-                    temp1 = (Vector<String>)classement.get(k);
-                    for(; k >= 0; k--) {
-                        if(k-1 != -1) {
-                            temp2 = (Vector<String>)classement.get(k-1);
-                            classement.set(k-1, temp1);
-                        }
-                        temp1 = temp2;
-                    }
-
-                    classement.set(j, film);
-                }
-            }
-            */
         }
         catch(SAXParseException e) {}
         catch(ParserConfigurationException e) { e.printStackTrace(); }
