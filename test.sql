8546ebf36eb433b334a6c3c74c6b6f9e2d1d7d91
Hugo-Fr-HEPL <78432085+Hugo-Fr-HEPL@users.noreply.github.com>
2021-11-17 09:07:53 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -11,33 +11,53 @@ GRANT EXECUTE ON SYS.DBMS_LOCK TO hugo;
 GRANT EXECUTE ON SYS.OWA_OPT_LOCK TO hugo;
 GRANT CREATE ANY DIRECTORY TO hugo;
 
-CREATE OR REPLACE DIRECTORY MYDIR AS 'D:\Programmation\SQL';
 
-CREATE TABLE etudiants_ext
+CREATE OR REPLACE DIRECTORY MYDIR AS '/home/hugo/Documents/Ecole/B3/E-Commerce/Labo-E-Commerce';
+
+
+CREATE TABLE ventes
 (
-    id NUMBER(4),
-    nom VARCHAR(30),
-    prenom VARCHAR(30),
-    dateNaiss DATE
-)
+    idVente NUMERIC(4),
+    idClient NUMERIC(4),
+    nomClient VARCHAR(30),
+    prenomClient VARCHAR(30),
+    mailClient VARCHAR(30),
+    
+    idMagasin NUMERIC(4),
+    nomMagasin VARCHAR(30),
+    codePostal NUMERIC(4),
 
+    --articles
+    dateAchat DATE,
+    URLTicket VARCHAR(30)
+)
 ORGANIZATION EXTERNAL
 (
     TYPE ORACLE_LOADER
     DEFAULT DIRECTORY MYDIR
     ACCESS PARAMETERS
     (
+        --commentaires
         RECORDS DELIMITED BY NEWLINE
         characterset "AL32UTF8"
         FIELDS TERMINATED BY ';'
         MISSING FIELD VALUES ARE NULL
         (
-            id unsigned integer external(2),
-            nom char(50),
-            prenom char(50),
-            dateNaiss char(10) date_format date mask "dd-mm-yyyy"
+            idVente unsigned integer external(2),
+            idClient unsigned integer external(2),
+            nomClient char(50),
+            prenomClient char(50),
+            mailClient char(50),
+            
+            idMagasin unsigned integer external(2),
+            nomMagasin char(50),
+            codePostal unsigned integer,
+
+            --articles
+            dateAchat char(10) date_format date mask "dd-mm-yyyy",
+            URLTicket char(50)
         )
     )
-    LOCATION('ext_tab_etudiants.txt')
+    LOCATION('ventes.txt')
 )
 REJECT LIMIT UNLIMITED;
